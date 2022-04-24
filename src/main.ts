8acaca0a6ad0c8baa38d5c5eb07446798603c009
devferx <ferkki@hotmail.com>
2022-04-23 22:15:07 -0400

    Observer y subscriber

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import { Observable } from "rxjs";
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<any> = {
+  next: (next) => console.log("siguiente [next]: ", next),
+  error: (error) => console.warn("error [obs]: ", error),
+  complete: () => console.info("complete [obs]"),
+};
 
 const obs$ = new Observable<string>((subs) => {
   subs.next("Hola");
@@ -7,12 +13,22 @@ const obs$ = new Observable<string>((subs) => {
   subs.next("Hola");
   subs.next("Mundo");
 
+  // Forzar un error
+  // const a = undefined;
+  // a.nombre = "Fernando";
+
   subs.complete();
 
   subs.next("Hola");
   subs.next("Mundo");
 });
 
-obs$.subscribe(console.log);
+// obs$.subscribe({
+//   next: (next) => console.log("next: ", next),
+//   error: (error) => console.warn("error: ", error),
+//   complete: () => console.info("complete"),
+// });
+
+obs$.subscribe(observer);
 
 export {};
