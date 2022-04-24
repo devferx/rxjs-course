068f9332ea4bd7341549b8a56bd95e02f45ca5fd
devferx <ferkki@hotmail.com>
2022-04-23 22:05:21 -0400

    Nuestro primer observable

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
+import { Observable } from "rxjs";
+
+const obs$ = new Observable<string>((subs) => {
+  subs.next("Hola");
+  subs.next("Mundo");
+
+  subs.next("Hola");
+  subs.next("Mundo");
+
+  subs.complete();
+
+  subs.next("Hola");
+  subs.next("Mundo");
+});
+
+obs$.subscribe(console.log);
+
 export {};
